bb1c281622d867765f38b7f221332f9e9e0b787c
Mea_Kotuba <74920484+Meakotuba@users.noreply.github.com>
2024-03-08 12:34:49 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ DC_PIN = 7
 CS_PIN = 6
 CLK_PIN = 5
 DIN_PIN = 4 
-
+CHG = ADC(Pin(19))
+CHG.atten(ADC.ATTN_11DB)
 ps2_y = ADC(Pin(18))
 ps2_y.atten(ADC.ATTN_11DB)
 ps2_x = ADC(Pin(17))
 ps2_x.atten(ADC.ATTN_11DB)
 uart=UART(2,9600,rx=48,tx=45) 
-
+CHG_ps = CHG.read()
 
 
 spi = SPI(1, baudrate=31250000, sck=Pin(CLK_PIN), mosi=Pin(DIN_PIN))
@@ -34,7 +35,13 @@ rs = 0
 T = 0
 H = 0
 while True:
-    
+    CHG_ps = CHG.read()
+    if CHG_ps < 200:
+        l = 5
+        r = 1
+    else:
+        l = 4
+        r = 2
     val_y = ps2_y.read()
     val_x = ps2_x.read()
     #Direction
@@ -42,16 +49,16 @@ while True:
         val_y = 3
         disp_y = "None"
     elif val_y > 2100 and val_y <= 3000:
-        val_y = 1#2
+        val_y = r#2
         disp_y = "<-  "
     elif val_y > 3000:
-        val_y = 1
+        val_y = r#1
         disp_y = "<---"
     elif val_y < 1900 and val_y >= 1000:
-        val_y = 5#4
+        val_y = l#4
         disp_y = "->  "
     elif val_y < 1000:
-        val_y = 5 
+        val_y = l#5 
         disp_y = "--->"
     # Speed
     if val_x >= 1900 and val_x <= 2200:
@@ -76,7 +83,7 @@ while True:
         old_x = val_x
         pr = "X:"+str(val_x)
         tft.text(font,pr, 10, 0, st7789.color565(255,255,255), st7789.color565(0,0,0))
-        pr = "Y:"+str(disp_y)
+        pr = "Y:"+str(val_y)
         tft.text(font,pr, 10, 50, st7789.color565(255,255,255), st7789.color565(0,0,0))
     if uart.any():
         rs = uart.read(7)
@@ -96,16 +103,16 @@ while True:
              val_y = 3
              disp_y = "None"
         elif val_y > 2100 and val_y <= 3000:
-             val_y = 1#2
+             val_y = r#2
              disp_y = "<-  "
         elif val_y > 3000:
-             val_y = 1
+             val_y = r#1
              disp_y = "<---"
         elif val_y < 1900 and val_y >= 1000:
-             val_y = 5#4
+             val_y = l#4
              disp_y = "->  "
         elif val_y < 1000:
-             val_y = 5
+             val_y = l#5
              disp_y = "--->"
     # Speed
         if val_x >= 1900 and val_x <= 2200:
@@ -125,11 +132,12 @@ while True:
         uart.write(send)#.encode('ascii'))
         pr = "X:"+str(val_x)
         tft.text(font,pr, 10, 0, st7789.color565(255,255,255), st7789.color565(0,0,0))
-        pr = "Y:"+str(disp_y)
+        pr = "Y:"+str(val_y)
         tft.text(font,pr, 10, 50, st7789.color565(255,255,255), st7789.color565(0,0,0))
         
     pr = "Temp:"+ str(T)
     tft.text(font,pr, 10, 100, st7789.color565(255,255,255), st7789.color565(0,0,0))
     pr = "H:"+ str(H)
     tft.text(font,pr, 10, 150, st7789.color565(255,255,255), st7789.color565(0,0,0))
-       
+    pr ="CHG:"+str(CHG_ps)
+    tft.text(font,pr, 10, 200, st7789.color565(255,255,255), st7789.color565(0,0,0))
